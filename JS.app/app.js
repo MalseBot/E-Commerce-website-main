2fc27dfbfda1f587364aad8c0016f5dcb04c391d
MALSE <eslamwtf@gmail.com>
2023-02-03 04:31:47 +0200

    added products details window

--- JS.app/app.js
+++ JS.app/app.js
@@ -3,17 +3,43 @@
     let Pp2 =document.getElementById("Pp2");
     let P1 =document.getElementById("page1");
     let P2 =document.getElementById("page2");
-function pages2() {
-    // let Ppn=Pp[Pp.length-1]
-    Pp2.classList.add("disabled")
-    Pp1.classList.remove("disabled")
+    function pages1() {
+    Pp1.classList.add("disabled")
+    Pp2.classList.remove("disabled") 
     P2.classList.add("visually-hidden")
     P1.classList.remove("visually-hidden")
 }
-function pages1() {
-    Pp1.classList.add("disabled")
-    Pp2.classList.remove("disabled") 
+function pages2() {
+    Pp2.classList.add("disabled")
+    Pp1.classList.remove("disabled")
     P1.classList.add("visually-hidden")
     P2.classList.remove("visually-hidden")
 }
 
+// product details images
+let mainImg = document.getElementById("mainImg");
+let smalling =document.getElementsByClassName("smalling");
+
+smalling[0].onclick= function(){
+    mainImg.src=smalling[0].src;
+}
+smalling[1].onclick= function(){
+    mainImg.src=smalling[1].src;
+}
+smalling[2].onclick= function(){
+    mainImg.src=smalling[2].src;
+}
+smalling[3].onclick= function(){
+    mainImg.src=smalling[3].src;
+}
+
+let proInfo = document.getElementById("proInfo")
+const proDetails =document.getElementsByClassName("prodetails")
+
+function hideDets(){
+    proInfo.classList.add("visually-hidden");
+}
+
+function revealDets(){
+    proInfo.classList.remove("visually-hidden")
+}
